03f387ee9ca2cfbe5a7c151efee65a86d51072c1
Victor <oladimejivictor611@gmail.com>
2024-10-07 17:12:09 +0100

    update

---
+++ Documents/bytebuilder/python classworks/tax_rates.py
@@ -0,0 +1,17 @@
+print("0-> Single filers,\n1-> married filing jointly,\n2-> married filing separately,\n3-> head of households\n") 
+	
+number = float(input("Enter the filing status "))
+taxable = float(input("Enter taxable income  ")) 
+taxs = 0
+if number == 0:
+	if taxable>0 and taxable < 8350 :
+		taxs = taxable * 0.1
+		print(taxs)
+	elif taxable > 8351 and taxable < 33950:
+		tax = 8350 * 0.10
+		taxs -= 8350
+		tab = tax * 0.15
+		table = tax + tab
+		print(table)
+	
+		
